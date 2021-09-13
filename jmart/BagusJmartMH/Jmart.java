98cee0e12fbecf09923c20004c679ec1bd3f7817
BagusNurhudaUI <bagus.nurhuda@ui.ac.id>
2021-09-13 17:25:37 +0700

    new class driver

--- jmart/BagusJmartMH/Jmart.java
+++ jmart/BagusJmartMH/Jmart.java
@@ -9,27 +9,74 @@ package BagusJmartMH;
  */
 public class Jmart
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Jmart
-     */
-    public Jmart()
+    public static void main (String args[])
     {
-        // initialise instance variables
-        x = 0;
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    
+    public static int getPromo()
     {
-        // put your code here
-        return x + y;
+        return 0;
+    }
+    
+    public static String getCustomer()
+    {
+        return "oop";
+    }
+    
+    public static float getDiscountPercentage(int before, int after)
+    {
+        if (before < after){
+            return 0.0f;
+        } 
+        int potHarga = before - after ;
+        float floatPotHarga = potHarga;
+        float floatBefore = before;
+        float percentage = floatPotHarga/floatBefore * 100;
+        return percentage;
+    }
+    
+    public static int getDiscountedPrice(int price, float discountPercentage)
+    {
+        float disc;
+        if (discountPercentage > 100.0f){
+            disc = 100.0f;
+        }else{
+            disc = discountPercentage;
+        }
+        float discPrice;
+        discPrice = price - (disc * price /100);
+        int intDiscPrice = (int) discPrice;
+        return intDiscPrice;
+        
+    }
+    
+    public static int getOriginalPrice(int discountedPrice, float discountPercentage)
+    {
+        float floatDiscountedPrice = discountedPrice;
+        float originalPrice = (floatDiscountedPrice * 100)/(100 - discountPercentage) ;
+        int intOriginalPrice = (int) originalPrice;
+        return intOriginalPrice;
+    }
+    
+    public static float getCommissionMultiplier()
+    {
+        return 0.05f;
+    }
+    
+    public static int getAdjustedPrice(int price)
+    {
+        float newPrice = price * getCommissionMultiplier();
+        int intNewPrice = (int) newPrice;
+        price = price + intNewPrice;
+        return price;
+    }
+    
+    public static int getAdminFree(int price)
+    {
+        float floatPrice = price;
+        float fee = floatPrice * getCommissionMultiplier();
+        int lastFee = (int) fee;
+        return lastFee;
     }
 }
