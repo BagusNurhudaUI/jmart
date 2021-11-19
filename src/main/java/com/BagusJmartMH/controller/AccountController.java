bfd77d5eeeec891c2cbc82255e5bc1d26b0da94b
BagusNurhudaUI <bagus.nurhuda@ui.ac.id>
2021-11-19 22:38:00 +0700

    Perubahan TP modul 8 - Bagus Nurhuda

---
+++ src/main/java/com/BagusJmartMH/controller/AccountController.java
@@ -0,0 +1,28 @@
+// TODO sesuaikan dengan package Anda: package com.alvinJmartRK.controller;
+package controller;
+
+// TODO sesuaikan dengan package Anda: import com.alvinJmartRK.Account;
+import com.BagusJmartMH.Account;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController 
+{
+	@GetMapping
+	String index() { return "account page"; }
+	
+	@PostMapping("/register")
+	Account register
+	(
+		@RequestParam String name,
+		@RequestParam String email,
+		@RequestParam String password
+	)
+	{
+		return new Account(name, email, password, 0);
+	}
+	
+	@GetMapping("/{id}")
+	String getById(@PathVariable int id) { return "account id " + id + " not found!"; }
+}
